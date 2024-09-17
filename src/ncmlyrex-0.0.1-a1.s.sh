f6f2565be4b51add02c7b9ac9b257ebe288fbe64
Tarikko-ScetayhChan <weychon_lyon@outlook.com>
2024-09-17 20:46:20 +0800

    v

--- src/ncmlyrex-0.0.1-a1.s.sh
+++ src/ncmlyrex-0.0.1-a1.s.sh
@@ -1,2 +1,40 @@
 #!/bin/bash
 
+function Ncmlyrex_PrintUsage() {
+    echo "Usage: ncmlyrex -i <song id> -o <output>"
+}
+
+while getopts ":i:o:" OPT; do
+    case "$OPT" in
+        i)
+            Ncmlyrex_SongId="$OPTARG"
+            ;;
+        o)
+            Ncmlyrex_Output="$OPTARG"
+            ;;
+        :)
+            echo "$0: argument expected after \`-$OPTARG'"
+            exit 1
+            ;;
+        *)
+            Ncmlyrex_PrintUsage
+            exit 1
+            ;;
+        ?)
+            echo "$0: invalid option \`-$OPTARG'"
+            exit 2
+            ;;
+    esac
+done
+
+if [ -f "$Ncmlyrex_Input" ]; then
+    echo "$0: file exists: \`$Ncmlyrex_Output'"
+    exit 1
+fi
+
+if ! curl "https://music.163.com/#/song?id=${Ncmlyrex_SongId}" > "$Ncmlyrex_Output.html"; then
+    echo "$0: failed to curl \`https://music.163.com/#/song?id=${Ncmlyrex_SongId}' into \`$Ncmlyrex_Output.html'"
+    exit 1
+fi
+
+cat -n $Ncmlyrex_Output.html
